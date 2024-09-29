da7291c96935fa375fdff86d89b2b5761846b005
pggbi <pggbi@AESTIVALIS>
2024-09-29 16:24:54 +0900

    Hallo World!

--- JavaTrainingSection4/src/md42/Md42.java
+++ JavaTrainingSection4/src/md42/Md42.java
@@ -15,8 +15,8 @@ public class Md42 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		// 配列宣言
 		String[] goods = new String[3];
-		String[] price = new String[3];
-		String[] pay = new String[3];
+		int[] price = new int[3];
+		int[] pay = new int[3];
 		// 配列への代入
 		for (int i = 0; i < goods.length; i++) {
 			System.out.println("商品を入力してください。");
@@ -24,39 +24,25 @@ public class Md42 {
 			goods[i] = str1;
 			System.out.println("単価を入力してください。");
 			String str2 = br.readLine();
-			price[i] = str2;
+			price[i] = Integer.parseInt(str2);
 			System.out.println("支払金額を入力してください。");
 			String str3 = br.readLine();
-			pay[i] = str3;
-		}
-		// String配列をint配列に変換
-		int[] price2 = new int[price.length];
-		for (int j = 0; j < price.length; j++) {
-			price2[j] = Integer.parseInt(price[j]);
-		}
-		int[] pay2 = new int[pay.length];
-		for (int k = 0; k < pay.length; k++) {
-			pay2[k] = Integer.parseInt(pay[k]);
+			pay[i] = Integer.parseInt(str3);
 		}
 		// 商品、単価、支払い金額の表示
-		for (int l = 0; l < goods.length; l++) {
-			if (price2[l] == pay2[l]) {
-				System.out.println(goods[l] + "\t" + price[l] + "円" + "\t" + "支払い過不足無し");
-			}else if (price2[l] > pay2[l]) {
-				System.out.println(goods[l] + "\t" + price[l] + "円" + "\t" + "支払い不足");
-			}else {
-				System.out.println(goods[l] + "\t" + price[l] + "円" + "\t" + "支払い超過");
-			}
-		}
-		// 単価、支払い金額差額の計算
 		int sum1 = 0;
 		int sum2 = 0;
-		for (int m = 0; m < price.length; m++) {
-			sum1 += price2[m];
+		for (int i = 0; i < goods.length; i++) {
+			if (price[i] == pay[i]) {
+				System.out.println(goods[i] + "\t" + price[i] + "円" + "\t" + "支払い過不足無し");
+			} else if (price[i] > pay[i]) {
+				System.out.println(goods[i] + "\t" + price[i] + "円" + "\t" + "支払い不足");
+			} else {
+				System.out.println(goods[i] + "\t" + price[i] + "円" + "\t" + "支払い超過");
+			}
+			sum1 += price[i];
+			sum2 += pay[i];
 		}
-		for (int n = 0; n < pay.length; n++) {
-			sum2 += pay2[n];
-		}
-		System.out.println("全体の不足金：" + (sum1-sum2) + "円");
+		System.out.println("全体の不足金:" + (sum1 - sum2) + "円");
 	}
 }
