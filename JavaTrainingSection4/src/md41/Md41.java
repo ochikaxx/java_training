8e55f750d908168a06f8200daa8cbc5acdc40e79
pggbi <pggbi@AESTIVALIS>
2024-10-02 22:40:45 +0900

    Hello World!

--- JavaTrainingSection4/src/md41/Md41.java
+++ JavaTrainingSection4/src/md41/Md41.java
@@ -8,15 +8,15 @@ public class Md41 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String[][] guest = {
-				{"中村", "田島", "小澤", "大西"},
-				{"5000円", "3000円", "4500円", "6000円"},
-				{"2000円", "0円", "0円", "3500円"}
+		String[] guest = {"中村", "田島", "小澤", "大西"};
+		int[][] fee = {
+				{5000, 3000, 4500, 6000},
+				{2000, 0, 0, 3500}
 		};
 		int count = 0;
 		for(int i = 0; i < 4; i++) {
-			System.out.println("契約者名:" + guest[0][i] + "\t" + "保険料:" + guest[1][i]);
-			if(guest[2][i] != ("0円")) {
+			System.out.println("契約者名:" + guest[i] + "\t" + "保険料:" + fee[0][i] + "円");
+			if(fee[1][i] > 0) {
 				count++;
 			}
 		}
