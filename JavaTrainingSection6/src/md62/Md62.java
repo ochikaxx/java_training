451206d4be10974ae622c578ad63c6533adda67a
pggbi <pggbi@AESTIVALIS>
2024-11-01 22:31:52 +0900

    Hello World!

--- JavaTrainingSection6/src/md62/Md62.java
+++ JavaTrainingSection6/src/md62/Md62.java
@@ -1,16 +1,18 @@
 package md62;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Md62 {
 	/**
 	 * mainメソッド
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Person[] people = {
-				new Person("山田", 26, "東京都大田区大森", "090-0000-0000"),
-				new Person("鈴木", 33, "和歌山県和歌山市", "090-1111-2222"),
-				new Person("田中", 41, "宮城県仙台市", "090-3333-4444")
-		};
+		List<Person> people = new ArrayList<Person>();
+		people.add(new Person("山田", 26, "東京都大田区大森", "090-0000-0000"));
+		people.add(new Person("鈴木", 33, "和歌山県和歌山市", "090-1111-2222"));
+		people.add(new Person("田中", 41, "宮城県仙台市", "090-3333-4444"));
 		
 		for (Person person : people) {
 			person.info();
