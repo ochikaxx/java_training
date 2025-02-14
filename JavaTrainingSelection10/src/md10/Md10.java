3a470c17f864e81cb1fb085a8c7a16f0c6c6a197
pggbi <pggbi@AESTIVALIS>
2025-02-14 23:43:36 +0900

    Hello World

--- JavaTrainingSelection10/src/md10/Md10.java
+++ JavaTrainingSelection10/src/md10/Md10.java
@@ -63,8 +63,8 @@ public class Md10 {
 
 			// 入力項目のチェックと出力
 			try {
-					process.check(name, age);
-					process.run(name, age);
+				process.check(name, age);
+				process.run(name, age);
 			} catch (CustomException e) {
 				System.out.println("エラー発生:" + e.getMessage());
 			}
